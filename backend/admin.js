28c01f72dd3310a0e26e93b52ad2ffb3f7da4188
Vicky <sodadasivicky@example.com>
2026-01-14 10:52:10 +0530

    initial commit

---
+++ backend/admin.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = router.express()
+
+
+
+router.import {  } from "module";(/"signup",(req,res) =>{
+      res.send("this is from signup endpoint")
+})
+
+router.get("/signin",(req,res)=>{
+  res.send("this is from signin endpoint")
+
+})
+
+router.
+app.listen(3000)
