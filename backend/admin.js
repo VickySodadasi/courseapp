569793838a1e6d74dd088bec3753756b99f8d333
Vicky <sodadasivicky@example.com>
2026-01-17 13:06:11 +0530

    modified admin.js file

--- backend/admin.js
+++ backend/admin.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
+router.post("/signup",req,res()=>{
+  res.send("hello this is from sign up end point");
+})
+
+router.post("/signin",req,res()=>{
+  res.send("this is from signin endpoint");
+
+})
+
+router.post("/deleteuser"req,res()=>{
+  res.send("this is from delete end point");
+})
